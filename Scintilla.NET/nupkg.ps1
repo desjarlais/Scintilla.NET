5df7e74d7f50f2af255f1c231743463738a6a154
Ahmet Sait <git@ahmetsait.com>
2024-05-04 16:04:11 +0300

    Fix #95: Implement solution-local NuGet registry
    - https://learn.microsoft.com/en-us/nuget/hosting-packages/local-feeds
    - https://stackoverflow.com/questions/10240029/how-do-i-install-a-nuget-package-nupkg-file-locally/48549013#48549013

---
+++ Scintilla.NET/nupkg.ps1
@@ -0,0 +1,24 @@
+param (
+	[Parameter(Mandatory=$true)][string]$NuGetPackageRoot,
+	[Parameter(Mandatory=$true)][string]$NuGetPackageSourceDir,
+	[Parameter(Mandatory=$true)][string]$PackageOutputPath,
+	[Parameter(Mandatory=$true)][string]$PackageId,
+	[Parameter(Mandatory=$true)][string]$PackageVersion
+)
+
+$PackageVersionList = $PackageVersion.Split('.')
+
+if ($PackageVersionList.Count -Eq 4 -And [int]::Parse($PackageVersionList[3]) -Eq 0) {
+	$PackageVersion = [string]::Join('.', $PackageVersionList[0..2])
+}
+
+$LocalNuGetPackageRegistry = [System.IO.Path]::GetFullPath((Join-Path $NuGetPackageSourceDir ..\registry))
+$PackagePath = Join-Path $PackageOutputPath "$PackageId.$PackageVersion.nupkg"
+
+$PackageInRegistry = Join-Path $LocalNuGetPackageRegistry $PackageId
+$PackageInRoot = Join-Path $NuGetPackageRoot $PackageId
+
+Remove-Item $PackageInRegistry -Recurse -Force -ErrorAction Ignore
+Remove-Item $PackageInRoot -Recurse -Force -ErrorAction Ignore
+
+nuget add $PackagePath -Source $LocalNuGetPackageRegistry
