d91708a5da711cb562d0a1bc1476d7de12c258eb
Ahmet Sait <git@ahmetsait.com>
2024-05-05 17:35:23 +0300

    Polish test app

--- Scintilla.NET.TestApp/FormMain.Designer.cs
+++ Scintilla.NET.TestApp/FormMain.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace Scintilla.NET.TestApp
+﻿namespace ScintillaNET.TestApp
 {
     partial class FormMain
     {
@@ -28,158 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.scintilla = new ScintillaNET.Scintilla();
-            this.msMain = new System.Windows.Forms.MenuStrip();
-            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
-            this.mnuTestMethod = new System.Windows.Forms.ToolStripMenuItem();
-            this.lexillaTestsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.listLexersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.odFile = new System.Windows.Forms.OpenFileDialog();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.scintilla1 = new ScintillaNET.Scintilla();
-            this.msMain.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // scintilla
-            // 
-            this.scintilla.AutoCMaxHeight = 9;
-            this.scintilla.BiDirectionality = ScintillaNET.BiDirectionalDisplayType.Disabled;
-            this.scintilla.CaretLineBackColor = System.Drawing.Color.White;
-            this.scintilla.CaretLineVisible = true;
-            this.scintilla.LexerName = null;
-            this.scintilla.Location = new System.Drawing.Point(0, 24);
-            this.scintilla.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.scintilla.Name = "scintilla";
-            this.scintilla.ScrollWidth = 6;
-            this.scintilla.Size = new System.Drawing.Size(331, 426);
-            this.scintilla.TabIndents = true;
-            this.scintilla.TabIndex = 0;
-            this.scintilla.UseRightToLeftReadingLayout = false;
-            this.scintilla.WrapMode = ScintillaNET.WrapMode.None;
-            this.scintilla.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.scintilla_MouseDoubleClick);
-            // 
-            // msMain
-            // 
-            this.msMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mnuFile,
-            this.mnuTestMethod,
-            this.lexillaTestsToolStripMenuItem});
-            this.msMain.Location = new System.Drawing.Point(0, 0);
-            this.msMain.Name = "msMain";
-            this.msMain.Size = new System.Drawing.Size(800, 24);
-            this.msMain.TabIndex = 1;
-            this.msMain.Text = "menuStrip1";
-            // 
-            // mnuFile
-            // 
-            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mnuOpen,
-            this.mnuExit});
-            this.mnuFile.Name = "mnuFile";
-            this.mnuFile.Size = new System.Drawing.Size(37, 20);
-            this.mnuFile.Text = "File";
-            // 
-            // mnuOpen
-            // 
-            this.mnuOpen.Name = "mnuOpen";
-            this.mnuOpen.Size = new System.Drawing.Size(124, 22);
-            this.mnuOpen.Text = "Open File";
-            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
-            // 
-            // mnuExit
-            // 
-            this.mnuExit.Name = "mnuExit";
-            this.mnuExit.Size = new System.Drawing.Size(124, 22);
-            this.mnuExit.Text = "Exit";
-            this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
-            // 
-            // mnuTestMethod
-            // 
-            this.mnuTestMethod.Name = "mnuTestMethod";
-            this.mnuTestMethod.Size = new System.Drawing.Size(160, 20);
-            this.mnuTestMethod.Text = "Test some Scintilla method";
-            this.mnuTestMethod.Click += new System.EventHandler(this.mnuTestMethod_Click);
-            // 
-            // lexillaTestsToolStripMenuItem
-            // 
-            this.lexillaTestsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.listLexersToolStripMenuItem});
-            this.lexillaTestsToolStripMenuItem.Name = "lexillaTestsToolStripMenuItem";
-            this.lexillaTestsToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
-            this.lexillaTestsToolStripMenuItem.Text = "Lexilla Tests";
-            // 
-            // listLexersToolStripMenuItem
-            // 
-            this.listLexersToolStripMenuItem.Name = "listLexersToolStripMenuItem";
-            this.listLexersToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
-            this.listLexersToolStripMenuItem.Text = "List Lexers";
-            this.listLexersToolStripMenuItem.Click += new System.EventHandler(this.listLexersToolStripMenuItem_Click);
-            // 
-            // odFile
-            // 
-            this.odFile.DefaultExt = "*.*";
-            this.odFile.Filter = "All Files|*.*";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(364, 56);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 23);
-            this.textBox1.TabIndex = 2;
-            // 
-            // scintilla1
-            // 
-            this.scintilla1.AutoCMaxHeight = 9;
-            this.scintilla1.BiDirectionality = ScintillaNET.BiDirectionalDisplayType.Disabled;
-            this.scintilla1.CaretLineBackColor = System.Drawing.Color.White;
-            this.scintilla1.CaretLineVisible = true;
-            this.scintilla1.LexerName = null;
-            this.scintilla1.Location = new System.Drawing.Point(364, 96);
-            this.scintilla1.Name = "scintilla1";
-            this.scintilla1.ScrollWidth = 49;
-            this.scintilla1.Size = new System.Drawing.Size(374, 323);
-            this.scintilla1.TabIndents = true;
-            this.scintilla1.TabIndex = 3;
-            this.scintilla1.Text = "scintilla1";
-            this.scintilla1.UseRightToLeftReadingLayout = false;
-            this.scintilla1.WrapMode = ScintillaNET.WrapMode.None;
-            // 
-            // FormMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.scintilla1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.scintilla);
-            this.Controls.Add(this.msMain);
-            this.MainMenuStrip = this.msMain;
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.Name = "FormMain";
-            this.Text = "ScintillaNET.TestApp";
-            this.Shown += new System.EventHandler(this.FormMain_Shown);
-            this.msMain.ResumeLayout(false);
-            this.msMain.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+			this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+			this.scintilla = new ScintillaNET.Scintilla();
+			this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel_Version = new System.Windows.Forms.ToolStripStatusLabel();
+			this.statusStrip = new System.Windows.Forms.StatusStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator = new System.Windows.Forms.ToolStripSeparator();
+			this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.describeKeywordSetsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip = new System.Windows.Forms.MenuStrip();
+			this.lexersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+			this.statusStrip.SuspendLayout();
+			this.menuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog
+			// 
+			this.openFileDialog.Filter = "All Files|*.*";
+			// 
+			// scintilla
+			// 
+			this.scintilla._ScintillaManagedDragDrop = true;
+			this.scintilla.BorderStyle = ScintillaNET.BorderStyle.Fixed3DVisualStyles;
+			this.scintilla.CaretLineBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(232)))), ((int)(((byte)(255)))));
+			this.scintilla.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.scintilla.FoldLineStripColor = System.Drawing.Color.Gray;
+			this.scintilla.Font = new System.Drawing.Font("Consolas", 10.2F);
+			this.scintilla.LexerName = null;
+			this.scintilla.Location = new System.Drawing.Point(0, 28);
+			this.scintilla.Name = "scintilla";
+			this.scintilla.ScrollWidth = 1;
+			this.scintilla.Size = new System.Drawing.Size(914, 426);
+			this.scintilla.TabIndex = 2;
+			this.scintilla.TextChanged += new System.EventHandler(this.scintilla_TextChanged);
+			this.scintilla.KeyDown += new System.Windows.Forms.KeyEventHandler(this.scintilla_KeyDown);
+			// 
+			// toolStripStatusLabel
+			// 
+			this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+			this.toolStripStatusLabel.Size = new System.Drawing.Size(13, 20);
+			this.toolStripStatusLabel.Text = " ";
+			// 
+			// toolStripStatusLabel_Version
+			// 
+			this.toolStripStatusLabel_Version.Name = "toolStripStatusLabel_Version";
+			this.toolStripStatusLabel_Version.Size = new System.Drawing.Size(886, 20);
+			this.toolStripStatusLabel_Version.Spring = true;
+			this.toolStripStatusLabel_Version.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// statusStrip
+			// 
+			this.statusStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel,
+            this.toolStripStatusLabel_Version});
+			this.statusStrip.Location = new System.Drawing.Point(0, 454);
+			this.statusStrip.Name = "statusStrip";
+			this.statusStrip.Size = new System.Drawing.Size(914, 26);
+			this.statusStrip.SizingGrip = false;
+			this.statusStrip.TabIndex = 3;
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.toolStripSeparator,
+            this.saveToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+			this.fileToolStripMenuItem.Text = "&File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+			this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+			this.openToolStripMenuItem.Text = "&Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator
+			// 
+			this.toolStripSeparator.Name = "toolStripSeparator";
+			this.toolStripSeparator.Size = new System.Drawing.Size(178, 6);
+			// 
+			// saveToolStripMenuItem
+			// 
+			this.saveToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripMenuItem.Image")));
+			this.saveToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+			this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+			this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+			this.saveToolStripMenuItem.Text = "&Save";
+			this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+			// 
+			// toolsToolStripMenuItem
+			// 
+			this.toolsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.describeKeywordSetsToolStripMenuItem,
+            this.optionsToolStripMenuItem});
+			this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+			this.toolsToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+			this.toolsToolStripMenuItem.Text = "&Tools";
+			// 
+			// describeKeywordSetsToolStripMenuItem
+			// 
+			this.describeKeywordSetsToolStripMenuItem.Name = "describeKeywordSetsToolStripMenuItem";
+			this.describeKeywordSetsToolStripMenuItem.Size = new System.Drawing.Size(243, 26);
+			this.describeKeywordSetsToolStripMenuItem.Text = "Describe &Keyword Sets";
+			this.describeKeywordSetsToolStripMenuItem.Click += new System.EventHandler(this.describeKeywordSetsToolStripMenuItem_Click);
+			// 
+			// optionsToolStripMenuItem
+			// 
+			this.optionsToolStripMenuItem.Enabled = false;
+			this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+			this.optionsToolStripMenuItem.Size = new System.Drawing.Size(243, 26);
+			this.optionsToolStripMenuItem.Text = "&Options";
+			// 
+			// helpToolStripMenuItem
+			// 
+			this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+			this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+			this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+			this.helpToolStripMenuItem.Text = "&Help";
+			// 
+			// aboutToolStripMenuItem
+			// 
+			this.aboutToolStripMenuItem.Enabled = false;
+			this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+			this.aboutToolStripMenuItem.Size = new System.Drawing.Size(142, 26);
+			this.aboutToolStripMenuItem.Text = "&About...";
+			// 
+			// menuStrip
+			// 
+			this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+			this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.lexersToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+			this.menuStrip.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip.Name = "menuStrip";
+			this.menuStrip.Size = new System.Drawing.Size(914, 28);
+			this.menuStrip.TabIndex = 1;
+			// 
+			// lexersToolStripMenuItem
+			// 
+			this.lexersToolStripMenuItem.Name = "lexersToolStripMenuItem";
+			this.lexersToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+			this.lexersToolStripMenuItem.Text = "Lexers";
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.ClientSize = new System.Drawing.Size(914, 480);
+			this.Controls.Add(this.scintilla);
+			this.Controls.Add(this.statusStrip);
+			this.Controls.Add(this.menuStrip);
+			this.MainMenuStrip = this.menuStrip;
+			this.Margin = new System.Windows.Forms.Padding(5, 3, 5, 3);
+			this.Name = "FormMain";
+			this.Text = "Scintilla.NET Test App";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+			this.Shown += new System.EventHandler(this.FormMain_Shown);
+			this.statusStrip.ResumeLayout(false);
+			this.statusStrip.PerformLayout();
+			this.menuStrip.ResumeLayout(false);
+			this.menuStrip.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
         private ScintillaNET.Scintilla scintilla;
-        private System.Windows.Forms.MenuStrip msMain;
-        private System.Windows.Forms.ToolStripMenuItem mnuFile;
-        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
-        private System.Windows.Forms.ToolStripMenuItem mnuExit;
-        private System.Windows.Forms.OpenFileDialog odFile;
-        private System.Windows.Forms.ToolStripMenuItem mnuTestMethod;
-        private System.Windows.Forms.ToolStripMenuItem lexillaTestsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem listLexersToolStripMenuItem;
-        private System.Windows.Forms.TextBox textBox1;
-        private ScintillaNET.Scintilla scintilla1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_Version;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.ToolStripMenuItem lexersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem describeKeywordSetsToolStripMenuItem;
     }
 }
 
